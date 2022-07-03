33beb5ca25b2db291b6ad502717ba69b926a0aa8
haydee27 <otokeincognita@gmail.com>
2022-07-03 13:31:51 -0600

    listarCategorias listo

---
+++ src/java/Controller/Categorias.java
@@ -0,0 +1,64 @@
+
+package Controller;
+
+import DAO.CategoriaDAO;
+import DAO.CategoriaDAOImplementar;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "Categorias", urlPatterns = {"/categorias.do"})
+public class Categorias extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Categorias</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Categorias at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    protected void listaCategorias(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        CategoriaDAO categoria = new CategoriaDAOImplementar();
+        
+        HttpSession sesion = request.getSession(true);
+        sesion.setAttribute("lista", categoria.Listar());
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Vistas-Categorias/listarCategorias.jsp");
+        dispatcher.forward(request, response);
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        this.listaCategorias(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+ 
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
